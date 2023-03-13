dedc92e4042fc04d0df3688eaed00fa1a36702dc
Carlos Raez <cr.unfv@gmail.com>
2023-03-13 13:14:11 -0500

    Adding main view with Compose

---
+++ app/src/main/java/com/raezcorp/mynotes/uicompose/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.raezcorp.mynotes.uicompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.raezcorp.mynotes.ui.main.MainViewModel
+import com.raezcorp.mynotes.uicompose.main.Main
+import com.raezcorp.mynotes.uicompose.ui.theme.MyNotesTheme
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyNotesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Main()
+                }
+            }
+        }
+    }
+}
